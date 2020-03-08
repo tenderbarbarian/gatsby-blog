5a95b08d585d8abbda6ccbd8fc54e21a77d96b6e
Kasia Pohl <ksoloduc@gmail.com>
2020-03-08 17:30:52 +0100

    specify html lang attribute

--- src/components/head.js
+++ src/components/head.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { graphql, useStaticQuery } from 'gatsby';
 
+const lang = 'en';
 const Head = ({ title }) => {
 	const data = useStaticQuery(graphql`
 		query {
@@ -15,7 +16,12 @@ const Head = ({ title }) => {
 		}
 	`);
 	return (
-		<Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
+		<Helmet
+			title={`${title} | ${data.site.siteMetadata.title}`}
+			htmlAttributes={{
+				lang
+			}}
+		>
 			<meta name="description" content={data.site.siteMetadata.description} />
 			<meta name="author" content={data.site.siteMetadata.author} />
 		</Helmet>
